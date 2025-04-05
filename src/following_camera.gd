62a1e94d8215e16c221386fd82d46efa04d033d5
scrawach <scrawach@gmail.com>
2025-04-05 15:02:44 +0300

    chore: a little bit colors works, updated camera as orthogonal

--- src/following_camera.gd
+++ src/following_camera.gd
@@ -3,6 +3,12 @@ extends Camera3D
 
 @export var target: Node3D
 @export var offset: Vector3
+@export var get_offset_on_ready: bool
+
+
+func _ready() -> void:
+	if get_offset_on_ready:
+		offset = position - target.position
 
 func _process(delta: float) -> void:
 	if target == null:
