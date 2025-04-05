244775c1e749206eddb066f863a2a30aa9eaa3e4
scrawach <scrawach@gmail.com>
2025-04-05 19:01:52 +0300

    feature: paint hunger and eat bars, add tween for update these values

--- src/living_lift/reactive_bar.gd
+++ src/living_lift/reactive_bar.gd
@@ -3,10 +3,16 @@ extends HBoxContainer
 
 @onready var progress_bar: ProgressBar = %ProgressBar
 
+var tween: Tween
+
 func construct(value: ReactiveValue) -> void:
 	value.updated.connect(_on_value_updated)
 	_on_value_updated(value)
 
 func _on_value_updated(value: ReactiveValue) -> void:
+	if tween != null:
+		tween.kill()
+	
 	progress_bar.max_value = value.max
-	progress_bar.value = value.current
+	tween = create_tween()
+	tween.tween_property(progress_bar, "value", value.current, 0.5)
