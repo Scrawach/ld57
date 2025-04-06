eacf984dadadeb9b57c567624baa799ef4bb1032
scrawach <scrawach@gmail.com>
2025-04-06 19:40:16 +0300

    fix: remove auto-lod generation for rooms

--- src/living_lift/doors.gd
+++ src/living_lift/doors.gd
@@ -10,10 +10,20 @@ const OPEN_ANIMATION: String = "open"
 
 @onready var animation: AnimationPlayer = $AnimationPlayer
 
+var is_closed: bool = true
+
 func open() -> void:
+	if not is_closed:
+		return
+	
 	collision.disabled = true
 	animation.play(OPEN_ANIMATION)
+	is_closed = false
 
 func close() -> void:
+	if is_closed:
+		return
+	
 	collision.disabled = false
 	animation.play_backwards(OPEN_ANIMATION)
+	is_closed = true
